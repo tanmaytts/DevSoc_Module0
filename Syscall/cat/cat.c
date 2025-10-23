6c4e9335ed11acd4c45c521a989609bfda792943
tanmaytts <tanmaysrivastava12345@gmail.com>
2025-10-23 08:40:45 +0530

    Added cat.c simple implementation of cat using syscall open

--- Syscall/cat/cat.c
+++ Syscall/cat/cat.c
@@ -22,35 +22,57 @@
  * 5. Close the file and handle any potential errors.
  */
 int main(int argc, char *argv[]) {
-    // TODO: Step 1 - Check for the correct number of command-line arguments.
-    // If argc is not equal to 2, print a usage message to stderr
-    // (e.g., "Usage: %s <filename>\n") and exit with a status of 1.
+    
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1; // Return an error code.
+    }
+
+    
 
 
-    // TODO: Step 2 - Open the file.
-    // The filename is in argv[1].
-    // Use the open() system call to open it in read-only mode (O_RDONLY).
-    // Store the returned file descriptor in an integer variable.
-    // If open() returns -1, an error occurred. Use perror("Error opening file")
-    // to print the error and exit with a status of 1.
+    char *filepath = argv[1];
+    int fd = open(filepath, O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        return 1; // Return an error code.
+    }
 
 
-    // TODO: Step 3 & 4 - Read from the file and write to standard output.
-    // Create a buffer (an array of chars) of size BUFFER_SIZE.
-    // Use a loop with the read() system call to read from the file into the buffer.
-    // The read() call returns the number of bytes read.
-    // The loop should continue as long as read() returns a value greater than 0.
-    // Inside the loop, use the write() system call to write the buffer's contents
-    // to standard output (STDOUT_FILENO). The number of bytes to write is the
-    // value returned by read().
-    // Be sure to handle potential errors from both read() and write().
 
 
-    // TODO: Step 5 - Close the file.
-    // Use the close() system call to close the file descriptor.
-    // Handle any potential errors from close().
+    char buffer[BUFFER_SIZE];
+
+    int rd;
+
+     printf("Printing %s\n", argv[1]);
+
+    while ((rd = read(fd, buffer, BUFFER_SIZE)) > 0) {
+        int written = 0;
+        while (written < rd) {
+            int w = write(STDOUT_FILENO, buffer + written, rd - written);
+            if (w == -1) {
+                
+                perror("write");
+                close(fd);
+                return 1;
+            }
+            written += w;
+        }
+    }
+
+    if (rd == -1) {
+        perror("read");
+        close(fd);
+        return 1;
+    }
 
 
-    return 0; // Return 0 for success.
+
+
+    
+    close(fd);
+    return 0;
 }
+    
 
